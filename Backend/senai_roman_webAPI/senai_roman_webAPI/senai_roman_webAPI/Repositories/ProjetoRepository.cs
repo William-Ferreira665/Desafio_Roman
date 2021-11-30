c6cdd565f15a7f53ed89fc20a567d732f07214c8
William Nunes Ferreira <william.ferreira@outlook.com.br>
2021-11-30 09:01:20 -0300

    ae

---
+++ Backend/senai_roman_webAPI/senai_roman_webAPI/senai_roman_webAPI/Repositories/ProjetoRepository.cs
@@ -0,0 +1,27 @@
+﻿using senai_roman_webAPI.Contexts;
+using senai_roman_webAPI.Domains;
+using senai_roman_webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_roman_webAPI.Repositories
+{
+    public class ProjetoRepository : IProjetoRepository
+    {
+        RomanContext ctx = new RomanContext();
+
+        public void Cadastrar(Projeto novoProjeto)
+        {
+            ctx.Projetos.Add(novoProjeto);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Projeto> ListarTodos()
+        {
+            return ctx.Projetos.ToList();
+        }
+    }
+}
